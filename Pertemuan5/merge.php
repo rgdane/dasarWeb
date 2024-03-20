5ed2ca5c3d55d81330f3aff2df70b70a4c5034fb
rgdane <regadanew1@gmail.com>
2024-03-20 07:30:42 +0700

    Pertemuan 05 - Praktikum 7

---
+++ Pertemuan5/merge.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Cara 01</title>
+</head>
+<body>
+    <p>Tanggal Hari ini : <?php echo date("d M Y")?></p>
+</body>
+</html>

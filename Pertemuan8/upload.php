a33f3c540a04b28a6050edf3f5e64dab84427a1b
rgdane <regadanew1@gmail.com>
2024-04-12 13:51:34 +0700

    Pertemuan 08 - Praktikum 1

--- Pertemuan8/upload.php
+++ Pertemuan8/upload.php
@@ -1,22 +1,21 @@
 <?php
-    if (isset($_POST["submit"])) {
-        $targetDirectory = "uploads/"; // Direktori tujuan untuk menyimpan file
-        $targetFile = $targetDirectory . basename($_FILES["fileToUpload"]["name"]);
-        $fileType = strtolower(pathinfo($targetFile, PATHINFO_EXTENSION));
-        
-        $allowedExtensions = array("jpg", "jpeg", "png", "gif");
-        
-        $maxFileSize = 5 * 1024 * 1024;
-        
-        if (in_array($fileType, $allowedExtensions) && $_FILES["fileToUpload"]["size"] <= $maxFileSize) {
-            if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $targetFile)) {
-                echo "File berhasil diunggah.";
-                echo "<br><img src='$targetFile' alt='Gambar' style='width: 200px;height: auto;'>";
-            } else {
-                echo "Gagal mengunggah file.";
-            }
-            } else {
-                echo "File tidak valid atau melebihi ukuran maksimum yang diizinkan.";
-            }
+if (isset($_POST["submit"])) {
+    $targetDirectory = "documents/"; // Direktori tujuan untuk menyimpan dokumen
+    $targetFile = $targetDirectory . basename($_FILES["documentToUpload"]["name"]);
+    $documentFileType = strtolower(pathinfo($targetFile, PATHINFO_EXTENSION));
+
+    $allowedExtensions = array("txt", "pdf", "doc", "docx");
+
+    $maxFileSize = 10 * 1024 * 1024;
+
+    if (in_array($documentFileType, $allowedExtensions) && $_FILES["documentToUpload"]["size"] <= $maxFileSize) {
+        if (move_uploaded_file($_FILES["documentToUpload"]["tmp_name"], $targetFile)) {
+            echo "Dokumen berhasil diunggah.";
+        } else {
+            echo "Gagal mengunggah dokumen.";
+        }
+    } else {
+        echo "Jenis dokumen tidak valid atau melebihi ukuran maksimum yang diizinkan.";
     }
+}
 ?>

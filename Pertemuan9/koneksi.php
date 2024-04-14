b419668eec83995ea1a55ad6cedf2b1ede0be956
rgdane <regadanew1@gmail.com>
2024-04-14 17:13:46 +0700

    Pertemuan 09 - Praktikum 2

---
+++ Pertemuan9/koneksi.php
@@ -0,0 +1,18 @@
+<?php
+$conn = new mysqli("localhost","root","","prakwebdb");
+
+if ($conn -> connect_errno) {
+    echo "Failed to connect to MySQL: " . $mysqli -> connect_error;
+    exit();
+}
+
+// Membuat tabel user
+// $query = "CREATE TABLE `prakwebdb`.`user` (`id` INT NOT NULL , `username` VARCHAR(50) NOT NULL ,
+//     `password` VARCHAR(50) NOT NULL , PRIMARY KEY (`id`));";
+// mysqli_query($conn, $query);
+
+// // Menginput data ke tabel user
+// $query2 = "INSERT INTO `user` (`id`, `username`, `password`) VALUES (3, 'admin', MD5('123'));";
+// mysqli_query($conn, $query);
+
+?>

0d83e38389e4a7ed3f3803ce8d09b9bff6de92ef
rgdane <regadanew1@gmail.com>
2024-04-17 05:34:15 +0700

    Pertemuan 10 - Praktikum 3

--- pertemuan10/proses.php
+++ pertemuan10/proses.php
@@ -16,6 +16,21 @@ if($aksi == "tambah") {
     } else {
         echo "Gagal menambahkan data : " . mysqli_error($conn);
     }
+} else if ($aksi == "ubah") {
+    if (isset($_POST["id"])) {
+        $id = $_POST["id"];
+
+        $query = "UPDATE anggota SET nama='$nama', jenis_kelamin='$jenis_kelamin', alamat='$alamat', no_telp='$no_telp' WHERE id='$id'";
+
+        if (mysqli_query($conn, $query)) {
+            header("Location: index.php");
+            exit();
+        } else {
+            echo "Gaga; mengupdate data : " . mysqli_error($conn);
+        }
+    } else {
+        echo "ID tidak valid";
+    }
 }
 mysqli_close($conn);
-?>;
+?>
